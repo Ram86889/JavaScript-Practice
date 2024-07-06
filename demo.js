cd383c72ea331250ac202c941a4ff062edb832a2
Ram86889 <dogiparthiram@gmail.com>
2024-07-06 11:25:49 +0530

    js scripts

---
+++ demo.js
@@ -0,0 +1,6 @@
+document.write("<h1 style=color:red;>EXTERNAL FILE</h1>")
+document.write("<h1 align=right style=color:yellow;>RAM</h1>");
+document.write("<p style=color:orange;>enter the height");
+document.write("<hr>")
+document.write("marriage");
+document.write("<hr>");
